cb6fc0bd3579d839e1fa1c7820dedc11a33604d5
Rajesh Hegde <rajeshhegde180@gmail.com>
2019-11-17 22:12:26 +0530

    Added files

---
+++ server.c
@@ -0,0 +1,66 @@
+/*
+	Server Side
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <ctype.h>
+
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main()
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[1024];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     // if (argc < 2) {
+     //     fprintf(stderr,"ERROR, no port provided\n");
+     //     exit(1);
+     // }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi("3000"); //prot assigned here to 3000 / arg[1]
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+       
+       
+      while(1){
+     char message1[255], message2[255];
+ 	 n = write(newsockfd,"Send message to server",strlen("Send message to server"));         //Ask for Number 1     
+     if (n < 0) error("ERROR writing to socket");
+     read(newsockfd, message1, 256);					//Read Number 1
+     printf("Client - Message : %s\n" , message1);
+		
+	 printf("Send message to client");
+	 scanf("%s", message2);
+     write(newsockfd , message2 , 256);
+    }
+     close(newsockfd);
+     close(sockfd);
+     return 0; 
+}
+
